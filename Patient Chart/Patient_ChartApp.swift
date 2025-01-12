9494b6755f9cafb8a16901a793ac387b9c1ed1d5
Priyanka Shrestha <priyankaspace@gmail.com>
2025-01-12 11:14:13 -0800

    Initial Commit

---
+++ Patient Chart/Patient_ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patient_ChartApp.swift
+//  Patient Chart
+//
+//  Created by Priyanka Shrestha on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Patient_ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Patient ChartUITests/Patient_ChartUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Patient_ChartUITestsLaunchTests.swift
+//  Patient ChartUITests
+//
+//  Created by Priyanka Shrestha on 1/12/25.
+//
+
+import XCTest
+
+final class Patient_ChartUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
